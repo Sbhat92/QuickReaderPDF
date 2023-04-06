c1366d8e990ae9b41df04c3bcba9e27455a289c6
Sbhat92 <shreyasbhat92@gmail.com>
2023-04-05 21:33:48 -0400

    changing import statement in init

--- QuickReaderPDF/__init__.py
+++ QuickReaderPDF/__init__.py
@@ -1,1 +1,1 @@
-from .pdfeditor import pdf_reader,text_to_pdf,create_html,pdf_to_html,remove_html,format_file # noqa: E501
+from .pdfeditor import pdf_reader,text_to_pdf,create_html,pdf_to_html,remove_html,format_file # noqa
