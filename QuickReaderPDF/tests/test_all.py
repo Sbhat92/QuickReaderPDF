ba2d54bd1735c105b8d8855d6b820c3deed42c5b
Sbhat92 <shreyasbhat92@gmail.com>
2023-04-08 16:18:20 -0400

    changing wb to w in test pdf to html

--- QuickReaderPDF/tests/test_all.py
+++ QuickReaderPDF/tests/test_all.py
@@ -39,8 +39,8 @@ def test_pdf_to_html(tmp_path):
             
     file_path_html = os.path.join(tmp_path, 'test_file.html')
     file_path_pdf = os.path.join(tmp_path, 'test_file.pdf')
-    with open(file_path_html, 'wb') as f:
-        f.write(b'This is a test.')
+    with open(file_path_html, 'w') as f:
+        f.write('This is a test.')
 
     pdf_to_html(file_path_html,file_path_pdf)    
     assert os.path.isfile(file_path_pdf)
