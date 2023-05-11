4677eba22c282417a0b43f35e0d116666b3c08fb
Sbhat92 <shreyasbhat92@gmail.com>
2023-05-11 11:13:04 -0400

    conf file

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'QuickReaderPDF'
 copyright = '2023, Shreyas Bhat'
 author = 'Shreyas Bhat'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
