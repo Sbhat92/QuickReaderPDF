1dea7abc010e0351bd8a979b249c2aafe7c6c1ad
Sbhat92 <shreyasbhat92@gmail.com>
2023-05-12 17:39:56 -0400

    prepping for release 3

--- QuickReaderPDF/__init__.py
+++ QuickReaderPDF/__init__.py
@@ -1,4 +1,4 @@
 from .pdfeditor import url_parser,copy_url,html_to_pdf,read_html_file,create_bs_obj,bolden_html,write_html,remove_html,main_func # noqa
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
